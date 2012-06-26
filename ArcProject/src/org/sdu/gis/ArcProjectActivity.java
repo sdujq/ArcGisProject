ff5f9dc9f67c05cdf4831a88b7cbe3b1354d8c9f
Ju Qiang <840046209@qq.com>
2012-06-26 15:40:24 +0800

    test 4
    4
    
    Signed-off-by: Ju Qiang <840046209@qq.com>

--- ArcProject/src/org/sdu/gis/ArcProjectActivity.java
+++ ArcProject/src/org/sdu/gis/ArcProjectActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class ArcProjectActivity extends Activity {
-	
+	//sdujq
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
