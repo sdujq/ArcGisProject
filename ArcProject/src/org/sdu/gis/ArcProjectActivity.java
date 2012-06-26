e8cc3b9e7a951474341d56031edf742b849ce4d4
jobean <jobean@126.com>
2012-06-26 15:57:48 +0800

    test 5
    5
    
    Signed-off-by: jobean <jobean@126.com>

--- ArcProject/src/org/sdu/gis/ArcProjectActivity.java
+++ ArcProject/src/org/sdu/gis/ArcProjectActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class ArcProjectActivity extends Activity {
 	//sdujq
+	//jobean
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
