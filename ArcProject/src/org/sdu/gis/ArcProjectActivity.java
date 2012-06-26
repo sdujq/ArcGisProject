82bb40795a607ca9cb265661d2b466590855af54
Ju Qiang <840046209@qq.com>
2012-06-26 14:35:06 +0800

    init the project
    init the project as an Android Project with 5 ArcGis libs

---
+++ ArcProject/src/org/sdu/gis/ArcProjectActivity.java
@@ -0,0 +1,13 @@
+package org.sdu.gis;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArcProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
