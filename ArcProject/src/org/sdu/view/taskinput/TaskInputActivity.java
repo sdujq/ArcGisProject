de6d3c1e719a995e2827b8f70a3bc171b1699e66
sduwxl <378650730@qq.com>
2012-06-29 16:03:04 +0800

    task_input部分监听
    task_input部分监听

--- ArcProject/src/org/sdu/view/taskinput/TaskInputActivity.java
+++ ArcProject/src/org/sdu/view/taskinput/TaskInputActivity.java
@@ -6,7 +6,14 @@ import com.tgb.lk.ahibernate.annotation.Id;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 //11
@@ -14,9 +21,13 @@ public class TaskInputActivity extends Activity {
 	public Button bt_zhiding, bt_qingkong, bt_xuanzequyu, bt_xunjianquyu,
 			bt_baocunrenwu, bt_faburenwu;
 	public TextView tv_luduanming, tv_renwuleibie, tv_xunjianrenyuan,
-			tv_renwuneirong, tv_kaishishijian, tv_jiezhishijian,
+			tv_renwuneirong, tv_qx_kaishishijian, tv_qx_jiezhishijian,
 			tv_xunjianzhouqi, tv_gerenwu, tv_beizhu, tv_zhidingren,
-			tv_zhidingshijian;
+			tv_zhidingshijian, tv_kaishiriqi, tv_jiezhiriqi;
+	public EditText et_luduanming, et_xunjianrenyuan, et_renwuneirong,
+			et_beizhu;
+
+	public Spinner sp_selectTask, sp_kaishishijian, sp_jiezhishijian;
 
 	@Override
 	public void onCreate(Bundle saved) {
@@ -24,29 +35,156 @@ public class TaskInputActivity extends Activity {
 		setContentView(R.layout.task_input);
 
 		bt_zhiding = (Button) findViewById(R.id.t_button_zhiding);
+		bt_zhiding.setOnClickListener(new ZhidingListener());
+
 		bt_qingkong = (Button) findViewById(R.id.t_button_qingkong);
+		bt_qingkong.setOnClickListener(new QingkongListener());
+
 		bt_xuanzequyu = (Button) findViewById(R.id.t_button_xuanzequyu);
+		bt_xuanzequyu.setOnClickListener(new XuanzequyuListener());
+
 		bt_xunjianquyu = (Button) findViewById(R.id.t_button_xunjianquyu);
+		bt_xunjianquyu.setOnClickListener(new XunjianquyuListener());
+
 		bt_baocunrenwu = (Button) findViewById(R.id.t_button_baocunrenwu);
+		bt_baocunrenwu.setOnClickListener(new BaocunrenwuListener());
+
 		bt_faburenwu = (Button) findViewById(R.id.t_button_faburenwu);
-		
-		tv_luduanming=(TextView) findViewById(R.id.t_nameOfRoad);
-		tv_renwuleibie=(TextView) findViewById(R.id.t_renwuleibie);
-		tv_xunjianrenyuan=(TextView) findViewById(R.id.t_xunjianrenyuan);
-		tv_renwuneirong=(TextView) findViewById(R.id.t_renwuneirong);
-		tv_kaishishijian=(TextView) findViewById(R.id.t_Qx_kaishi);
-		tv_jiezhishijian=(TextView) findViewById(R.id.t_Qx_jiezhi);
-		tv_xunjianzhouqi=(TextView) findViewById(R.id.t_xunjianzhouqi);
-		tv_gerenwu=(TextView) findViewById(R.id.t_gerenwu);
-		tv_beizhu=(TextView) findViewById(R.id.t_beizhu);
-		tv_zhidingren=(TextView) findViewById(R.id.t_zhidingren);
-		tv_zhidingshijian=(TextView) findViewById(R.id.t_zhidingshijian);
-		
-		
-		
-		
-		
-		
+		bt_faburenwu.setOnClickListener(new FaburenwuListener());
+
+		tv_luduanming = (TextView) findViewById(R.id.t_nameOfRoad);
+		tv_renwuleibie = (TextView) findViewById(R.id.t_renwuleibie);
+		tv_xunjianrenyuan = (TextView) findViewById(R.id.t_xunjianrenyuan);
+		tv_renwuneirong = (TextView) findViewById(R.id.t_renwuneirong);
+		tv_qx_kaishishijian = (TextView) findViewById(R.id.t_Qx_kaishi);
+		tv_kaishiriqi = (TextView) findViewById(R.id.t_kaishiriqi);
+		tv_qx_jiezhishijian = (TextView) findViewById(R.id.t_Qx_jiezhi);
+		tv_jiezhiriqi = (TextView) findViewById(R.id.t_jiezhiriqi);
+		tv_xunjianzhouqi = (TextView) findViewById(R.id.t_xunjianzhouqi);
+		tv_gerenwu = (TextView) findViewById(R.id.t_gerenwu);
+		tv_beizhu = (TextView) findViewById(R.id.t_beizhu);
+		tv_zhidingren = (TextView) findViewById(R.id.t_zhidingren);
+		tv_zhidingshijian = (TextView) findViewById(R.id.t_zhidingshijian);
+
+		sp_selectTask = (Spinner) findViewById(R.id.t_spinner_selectTask);
+		sp_kaishishijian = (Spinner) findViewById(R.id.t_spinner_kaishishijian);
+		sp_jiezhishijian = (Spinner) findViewById(R.id.t_spinner_jiezhishijian);
+
+		tv_kaishiriqi.setText(R.string.kaishiriqi);
+		tv_jiezhiriqi.setText(R.string.jiezhiriqi);
+		tv_zhidingshijian.setText(R.string.zhidingren);
+		tv_zhidingren.setText(R.string.zhidingshijian);
 
 	}
+
+	// 按钮 制定 的监听
+	class ZhidingListener implements OnClickListener {
+
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			AnimationSet animationSet = new AnimationSet(true);
+			ScaleAnimation scaleAnimation = new ScaleAnimation(1, 1.3f, 1,
+					1.3f, Animation.RELATIVE_TO_SELF, 0.5f,
+					Animation.RELATIVE_TO_SELF, 0.5f);
+			animationSet.addAnimation(scaleAnimation);
+			animationSet.setFillBefore(true);
+			animationSet.setDuration(200);
+			bt_zhiding.startAnimation(animationSet);
+
+			System.out.println("制定");
+
+		}
+
+	}
+
+	// 按钮 清空 的监听
+	class QingkongListener implements OnClickListener {
+
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			AnimationSet animationSet = new AnimationSet(true);
+			ScaleAnimation scaleAnimation = new ScaleAnimation(1, 1.3f, 1,
+					1.3f, Animation.RELATIVE_TO_SELF, 0.5f,
+					Animation.RELATIVE_TO_SELF, 0.5f);
+			animationSet.addAnimation(scaleAnimation);
+			animationSet.setFillBefore(true);
+			animationSet.setDuration(200);
+			bt_qingkong.startAnimation(animationSet);
+
+		}
+
+	}
+
+	// 按钮 选择区域 的监听
+	class XuanzequyuListener implements OnClickListener {
+
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			AnimationSet animationSet = new AnimationSet(true);
+			ScaleAnimation scaleAnimation = new ScaleAnimation(1, 1.3f, 1,
+					1.3f, Animation.RELATIVE_TO_SELF, 0.5f,
+					Animation.RELATIVE_TO_SELF, 0.5f);
+			animationSet.addAnimation(scaleAnimation);
+			animationSet.setFillBefore(true);
+			animationSet.setDuration(200);
+			bt_xuanzequyu.startAnimation(animationSet);
+
+		}
+
+	}
+
+	// 按钮 巡检区域 的监听
+	class XunjianquyuListener implements OnClickListener {
+
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			AnimationSet animationSet = new AnimationSet(true);
+			ScaleAnimation scaleAnimation = new ScaleAnimation(1, 1.3f, 1,
+					1.3f, Animation.RELATIVE_TO_SELF, 0.5f,
+					Animation.RELATIVE_TO_SELF, 0.5f);
+			animationSet.addAnimation(scaleAnimation);
+			animationSet.setFillBefore(true);
+			animationSet.setDuration(200);
+			bt_xunjianquyu.startAnimation(animationSet);
+
+		}
+
+	}
+
+	// 按钮 保存任务 的监听
+	class BaocunrenwuListener implements OnClickListener {
+
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			AnimationSet animationSet = new AnimationSet(true);
+			ScaleAnimation scaleAnimation = new ScaleAnimation(1, 1.3f, 1,
+					1.3f, Animation.RELATIVE_TO_SELF, 0.5f,
+					Animation.RELATIVE_TO_SELF, 0.5f);
+			animationSet.addAnimation(scaleAnimation);
+			animationSet.setFillBefore(true);
+			animationSet.setDuration(200);
+			bt_baocunrenwu.startAnimation(animationSet);
+
+		}
+
+	}
+
+	// 按钮 发布任务 的监听
+	class FaburenwuListener implements OnClickListener {
+
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			AnimationSet animationSet = new AnimationSet(true);
+			ScaleAnimation scaleAnimation = new ScaleAnimation(1, 1.3f, 1,
+					1.3f, Animation.RELATIVE_TO_SELF, 0.5f,
+					Animation.RELATIVE_TO_SELF, 0.5f);
+			animationSet.addAnimation(scaleAnimation);
+			animationSet.setFillBefore(true);
+			animationSet.setDuration(200);
+			bt_faburenwu.startAnimation(animationSet);
+
+		}
+
+	}
+
 }
