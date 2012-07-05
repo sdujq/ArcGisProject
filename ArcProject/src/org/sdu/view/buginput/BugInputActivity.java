1a9071bfd5bef3bef853953123d40ff764ff363f
Ju Qiang <840046209@qq.com>
2012-07-06 02:52:46 +0800

    add loaction service by gps apn and wifi

--- ArcProject/src/org/sdu/view/buginput/BugInputActivity.java
+++ ArcProject/src/org/sdu/view/buginput/BugInputActivity.java
@@ -16,14 +16,18 @@ import org.sdujq.map.InputActivity;
 import org.sdujq.map.Photo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
@@ -35,6 +39,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.maxtech.common.AddressTask;
+import com.maxtech.common.GpsActivity;
+import com.maxtech.common.gps.GpsTask;
+import com.maxtech.common.gps.GpsTaskCallBack;
+import com.maxtech.common.gps.GpsTask.GpsData;
+import com.maxtech.common.gps.IAddressTask.MLocation;
+
 public class BugInputActivity extends Activity {
 	Context mContext = null;
 
@@ -44,8 +55,10 @@ public class BugInputActivity extends Activity {
 			textView6, textView7;
 	public EditText editText1, editText3, editText4, editText5, editText6;
 	public Spinner bugtype_spinner;
-	public Button button1, button2, button3, button4, editPhoto, clear;
+	public Button button1, button2, button3, button4, editPhoto, clear, apn,
+			gps, wifi;
 	public ImageView image;
+	private AlertDialog dialog = null;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -66,12 +79,11 @@ public class BugInputActivity extends Activity {
 		editText5 = (EditText) findViewById(R.id.guzhangdizhi_editText);
 		editText6 = (EditText) findViewById(R.id.wentibeizhu_editText);
 		image = (ImageView) findViewById(R.id.imageView1);
-		bugtype_spinner=(Spinner)findViewById(R.id.wentileibie_spinner);
+		bugtype_spinner = (Spinner) findViewById(R.id.wentileibie_spinner);
 		ArrayAdapter<BugType> adapter = new ArrayAdapter<BugType>(
-				BugInputActivity.this,
-				android.R.layout.simple_spinner_item);
-		List<BugType> typeList=(new BugTypeDao(this)).find();
-		for(BugType b:typeList){
+				BugInputActivity.this, android.R.layout.simple_spinner_item);
+		List<BugType> typeList = (new BugTypeDao(this)).find();
+		for (BugType b : typeList) {
 			adapter.add(b);
 		}
 		bugtype_spinner.setAdapter(adapter);
@@ -96,22 +108,24 @@ public class BugInputActivity extends Activity {
 			}
 		});
 
-
 		// 确定问题输入后，点击“发布”来发布故障
 		button4 = (Button) findViewById(R.id.fabu_button);
 		button4.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
-				Bug bug=new Bug();
+				Bug bug = new Bug();
 				bug.setAddress(editText5.getText().toString());
-				if(image.getDrawable()!=null){
-					Bitmap bitmap=BitmapTool.drawableToBitmap(image.getDrawable());
+				if (image.getDrawable() != null) {
+					Bitmap bitmap = BitmapTool.drawableToBitmap(image
+							.getDrawable());
 					bug.setAttachment(BitmapTool.Bitmap2Bytes(bitmap));
 				}
-				bug.setBugTypeId(((BugType)bugtype_spinner.getSelectedItem()).getId());
+				bug.setBugTypeId(((BugType) bugtype_spinner.getSelectedItem())
+						.getId());
 				bug.setContent(editText3.getText().toString());
-				bug.setState(""+0);
+				bug.setState("" + 0);
 				bug.setTag(editText6.getText().toString());
-				//TODO 
+				bug.setPoint(editText4.getText().toString());
+				// TODO
 				bug.setUserId(0);
 				new BugDao(BugInputActivity.this).insert(bug);
 				clear();
@@ -126,32 +140,78 @@ public class BugInputActivity extends Activity {
 			public void onClick(View v) {
 				PhotoDrawerTemp.shareBMP = BitmapTool.drawableToBitmap(image
 						.getDrawable());
-				Intent it=new Intent();
+				Intent it = new Intent();
 				it.setClass(BugInputActivity.this, PhotoDrawerTemp.class);
-				 BugInputActivity.this.startActivityForResult(it, 111);
+				BugInputActivity.this.startActivityForResult(it, 111);
 			}
 		});
-		
-		clear=(Button)findViewById(R.id.button5);
+
+		clear = (Button) findViewById(R.id.button5);
 		clear.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				clear();
 			}
 		});
-		
+
 		editText4.setOnLongClickListener(new OnLongClickListener() {
-			
+
 			@Override
 			public boolean onLongClick(View v) {
-				
+
 				return false;
+			}
+		});
+		dialog = new ProgressDialog(this);
+		dialog.setTitle("请稍等...");
+		dialog.setMessage("正在定位...");
+
+		apn = (Button) findViewById(R.id.apn);
+		wifi = (Button) findViewById(R.id.wifi);
+		gps = (Button) findViewById(R.id.gps);
+
+		apn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				do_apn();
+			}
+		});
+		gps.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				dialog.show();
+				GpsTask gpstask = new GpsTask(BugInputActivity.this,
+						new GpsTaskCallBack() {
+
+							@Override
+							public void gpsConnectedTimeOut() {
+								dialog.dismiss();
+								Toast.makeText(BugInputActivity.this, "GPS获取超时", 0).show();
+							}
+
+							@Override
+							public void gpsConnected(GpsData gpsdata) {
+								do_gps(gpsdata);
+								dialog.dismiss();
+							}
+
+						}, 3000);
+				gpstask.execute();
+			}
+		});
+		wifi.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				do_wifi();
 			}
 		});
 	}
 
-	public void clear(){
+	public void clear() {
 		editText1.setText("");
 		editText3.setText("");
 		editText4.setText("");
@@ -159,6 +219,7 @@ public class BugInputActivity extends Activity {
 		editText6.setText("");
 		image.setImageBitmap(null);
 	}
+
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == InputActivity.TAKE_PHOTO_REQUEST_CODE) {
 
@@ -193,7 +254,7 @@ public class BugInputActivity extends Activity {
 					e.printStackTrace();
 				}
 			}
-		}else if(requestCode==111&&resultCode==RESULT_OK){
+		} else if (requestCode == 111 && resultCode == RESULT_OK) {
 			ContentResolver cr = getContentResolver();
 			try {
 				InputStream in = cr.openInputStream(Uri.fromFile(new File(
@@ -204,5 +265,117 @@ public class BugInputActivity extends Activity {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private void do_apn() {
+		new AsyncTask<Void, Void, MLocation>() {
+
+			@Override
+			protected MLocation doInBackground(Void... params) {
+				MLocation location = null;
+				try {
+					location = new AddressTask(BugInputActivity.this,
+							AddressTask.DO_APN).doApnPost();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				if (location == null)
+					return null;
+				return location;
+			}
+
+			@Override
+			protected void onPreExecute() {
+				dialog.show();
+				super.onPreExecute();
+			}
+
+			@Override
+			protected void onPostExecute(MLocation result) {
+				if (result == null) {
+					Toast.makeText(BugInputActivity.this, "基站定位失败", 0).show();
+				} else {
+					editText4.setText(result.Latitude + "," + result.Longitude);
+				}
+				dialog.dismiss();
+				super.onPostExecute(result);
+			}
+
+		}.execute();
+	}
+
+	private void do_gps(final GpsData gpsdata) {
+		new AsyncTask<Void, Void, MLocation>() {
+
+			@Override
+			protected MLocation doInBackground(Void... params) {
+				MLocation location = null;
+				try {
+					Log.i("do_gpspost", "经纬度：" + gpsdata.getLatitude() + "----"
+							+ gpsdata.getLongitude());
+					location = new AddressTask(BugInputActivity.this,
+							AddressTask.DO_GPS).doGpsPost(
+							gpsdata.getLatitude(), gpsdata.getLongitude());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return location;
+			}
+
+			@Override
+			protected void onPreExecute() {
+				dialog.show();
+				super.onPreExecute();
+			}
+
+			@Override
+			protected void onPostExecute(MLocation result) {
+				if (result != null) {
+					editText4.setText(result.Latitude + "," + result.Longitude);
+				}
+				dialog.dismiss();
+				super.onPostExecute(result);
+			}
+
+		}.execute();
+	}
+
+	private void do_wifi() {
+		new AsyncTask<Void, Void, MLocation>() {
+
+			@Override
+			protected MLocation doInBackground(Void... params) {
+				MLocation location = null;
+				try {
+					location = new AddressTask(BugInputActivity.this,
+							AddressTask.DO_WIFI).doWifiPost();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				if (location == null)
+					return null;
+				return location;
+			}
+
+			@Override
+			protected void onPreExecute() {
+				dialog.show();
+				super.onPreExecute();
+			}
+
+			@Override
+			protected void onPostExecute(MLocation result) {
+				if (result != null) {
+					editText4.setText(result.Latitude + "," + result.Longitude);
+				} else {
+					Toast.makeText(BugInputActivity.this, "WIFI定位失败了", 0)
+							.show();
+				}
+
+				dialog.dismiss();
+				super.onPostExecute(result);
+			}
+
+		}.execute();
 	}
 }
