65222fbb2ea732a2595112f7ac8b5472c03acf8d
jobean <jobean@126.com>
2012-07-05 09:22:08 +0800

    修改了故障输入界面
    按钮功能简化并明确了

--- ArcProject/src/org/sdu/view/buginput/BugInputActivity.java
+++ ArcProject/src/org/sdu/view/buginput/BugInputActivity.java
@@ -3,23 +3,28 @@ package org.sdu.view.buginput;
 import org.sdu.gis.R;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class BugInputActivity extends Activity{
+	Context mContext = null;
 	
 	public TextView textView1,textView2,textView3,textView4,textView5,textView6,textView7;
 	public EditText editText1,editText3,editText4,editText5,editText6,editText7; 
     public Spinner bugtype_spinner;
 	public Button button1,button2,button3,button4;
 	
-	@Override
+	
 	public void onCreate(Bundle saved){
 		super.onCreate(saved);
 		setContentView(R.layout.debug);
+		mContext = this;
 		
 		textView1 = (TextView)findViewById(R.id.wentijianshu_textView);
 		textView2 = (TextView)findViewById(R.id.wentileibie_textView);
@@ -36,17 +41,37 @@ public class BugInputActivity extends Activity{
 		editText6 = (EditText)findViewById(R.id.wentibeizhu_editText);
 		editText7 = (EditText)findViewById(R.id.xianchangfujian_editText);
 		
+		//现场附件   获取图片（拍照片）
 		button1 = (Button)findViewById(R.id.xianchangfujian_button);
-		//button1.setOnClickListener(new XianchangfujianListener());
+		button1.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0){
+				
+			}
+		});
 		
+		//删除图片  重新获取
 		button2 = (Button)findViewById(R.id.shanchu_button);
-		//button2.setOnClickListener(new ShanchuListener());
+		button2.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0){
+				
+			}
+		});
 		
+		//对输入的问题页面进行保存
 		button3 = (Button)findViewById(R.id.baocun_button);
-		//button3.setOnClickListener(new baocunListener());
+		button3.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0){
+				
+			}
+		});
 		
+		//确定问题输入后，点击“发布”来发布故障
 		button4 = (Button)findViewById(R.id.fabu_button);
-		//button4.setOnClickListener(new fabuListener());
+		button4.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0){
+				
+			}
+		});
 		
 	}
 }
