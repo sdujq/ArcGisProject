d91fa574415569b3bda5183f1aa17baceda24390
Ju Qiang <840046209@qq.com>
2012-07-10 15:21:31 +0800

    try to pull to refresh

--- ArcProject/src/org/sdu/view/taskshow/TaskShowMhActivity.java
+++ ArcProject/src/org/sdu/view/taskshow/TaskShowMhActivity.java
@@ -1,33 +1,40 @@
 package org.sdu.view.taskshow;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.sdu.dao.TaskDao;
 import org.sdu.dao.UserDao;
+import org.sdu.dbaction.Action;
+import org.sdu.dbaction.TaskAction;
 import org.sdu.gis.R;
 import org.sdu.pojo.Task;
+import org.sdujq.map.PullToRefreshListView;
+import org.sdujq.map.PullToRefreshListView.OnRefreshListener;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class TaskShowMhActivity extends Activity {
 
-	ListView lv;
+	PullToRefreshListView lv;
 	public static TaskShowMhActivity currentActivity;
 	public Handler h = new Handler() {
 		@Override
 		public void handleMessage(Message m) {
-			lv.setAdapter(new TaskShowAdapter());
+			refreshData();
 		}
 	};
 
@@ -39,18 +46,38 @@ public class TaskShowMhActivity extends Activity {
 	public void onCreate(Bundle b) {
 		super.onCreate(b);
 		setContentView(R.layout.taskshowmh);
-		lv = (ListView) findViewById(R.id.listView1);
+		lv = (PullToRefreshListView) findViewById(R.id.listView1);
 		currentActivity = this;
 		lv.setAdapter(new TaskShowAdapter());
+		lv.setOnRefreshListener(new OnRefreshListener() {
+
+			@Override
+			public void onRefresh() {
+				new GetDataTask().execute();
+			}
+		});
 	}
 
 	class TaskShowAdapter extends BaseAdapter {
-
-		TaskDao taskDao = new TaskDao(TaskShowMhActivity.this);
+		int limit = 6;
+		int offset = 0;
+		Drawable d1, d2, d3;
 		List<Task> data;
+		List<Integer> idList;
 
 		public TaskShowAdapter() {
-			data = taskDao.find();
+			TaskAction tAction = new TaskAction(TaskShowMhActivity.this);
+			idList = tAction.getTaskIds(Action.currentUser.getId() + "");
+			Log.e("qq", idList.size() + "");
+			try {
+				data = tAction.getList(idList.subList(offset,
+						Math.min(limit, idList.size() - 1)));
+			} catch (Exception e) {
+				data = new ArrayList<Task>();
+			}
+			d1 = getResources().getDrawable(R.drawable.state_1);
+			d2 = getResources().getDrawable(R.drawable.state_2);
+			d3 = getResources().getDrawable(R.drawable.state_3);
 		}
 
 		@Override
@@ -70,6 +97,9 @@ public class TaskShowMhActivity extends Activity {
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView != null) {
+				return convertView;
+			}
 			View v = View.inflate(TaskShowMhActivity.this,
 					R.layout.task_show_item, null);
 			TextView tv1 = (TextView) v.findViewById(R.id.cake_name);
@@ -79,11 +109,11 @@ public class TaskShowMhActivity extends Activity {
 			ImageView icon = (ImageView) v.findViewById(R.id.task_icon);
 			final Task t = data.get(position);
 			if (t.getState().equals("3")) {
-				icon.setImageResource(R.drawable.state_3);
+				icon.setImageDrawable(d3);
 			} else if (t.getState().equals("2")) {
-				icon.setImageResource(R.drawable.state_2);
+				icon.setImageDrawable(d2);
 			} else if (t.getState().equals("1")) {
-				icon.setImageResource(R.drawable.state_1);
+				icon.setImageDrawable(d1);
 			}
 			tv1.setText("任务代号:" + t.getId());
 			tv2.setText("任务类型:" + t.getTaskType());
@@ -109,5 +139,30 @@ public class TaskShowMhActivity extends Activity {
 			return v;
 		}
 
+	}
+
+	private class GetDataTask extends AsyncTask<Void, Void, List<Task>> {
+
+		@Override
+		protected List<Task> doInBackground(Void... params) {
+			List<Task> lst = new ArrayList<Task>();
+			try {
+				Thread.sleep(500);
+				refreshData();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return lst;
+		}
+
+		@Override
+		protected void onPostExecute(List<Task> data) {
+			// mListItems.addFirst("Added after refresh...");
+
+			// Call onRefreshComplete when the list has been refreshed.
+			lv.onRefreshComplete();
+
+			super.onPostExecute(data);
+		}
 	}
 }
